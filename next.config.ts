35153003d1674d5f284ada3163da5c20bfd8d0a8
glissejm <glisse.jorge.27@unsch.edu.pe>
2025-03-03 11:27:17 -0500

    add config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/2048-in-react",
   images: {
     unoptimized: true, // Desactiva la optimización de imágenes para export estático
   },
