0b1380606797b92d1c6e2786c8fc7c187c6e93fe
SihomaSa <sandy.ochoa.27@unsch.edu.pe>
2026-01-05 03:20:39 -0500

    IWD 2026

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -4,7 +4,7 @@ const Container = ({ children }: { children: React.ReactNode }) => (
   <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
 );
 
-export default function Sponsors() {
+export function Sponsors() {
   return (
     <section
       id="sponsors"
